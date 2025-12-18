a4fd99aaa4133b87bbf251393fc775ad7167cb7f
Anshuman Sarin <42437399+ansh98@users.noreply.github.com>
2025-12-19 00:34:52 +0530

    Update VestingVault.t.sol

--- test/VestingVault.t.sol
+++ test/VestingVault.t.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/VestingVault.sol";
+import "../src/mocks/FeeToken.sol";
+
+contract VestingVaultTest is Test {
+
+    function testCliffBlocksClaim() public {
+        // test logic
+    }
+
+    function testPartialClaim() public {
+        // test logic
+    }
+
+    function testFullClaim() public {
+        // test logic
+    }
+
+    function testRevokeBeforeCliff() public {
+        // test logic
+    }
+}
 
