ec7ad41d786abd23f5850ea6b12cd60a4575938e
Anshuman Sarin <42437399+ansh98@users.noreply.github.com>
2025-12-18 17:06:20 +0530

    Update FeeToken.sol

--- src/mocks/FeeToken.sol
+++ src/mocks/FeeToken.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract FeeToken is ERC20 {
+    uint256 public constant FEE_BPS = 200; // 2%
+
+    constructor() ERC20("FeeToken", "FEE") {
+        _mint(msg.sender, 1_000_000 ether);
+    }
+
+    function _transfer(address from, address to, uint256 amount) internal override {
+        uint256 fee = (amount * FEE_BPS) / 10_000;
+        uint256 net = amount - fee;
+        super._transfer(from, to, net);
+        super._transfer(from, address(0xdead), fee);
+    }
+}
 
